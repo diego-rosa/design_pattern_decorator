04252302f9ea0c1ab6e2bde7bb1d84686d9ce282
diego-rosa <diego.jose.rosa.da.silva@gmail.com>
2022-07-12 01:44:04 -0300

    Adicionando o arquivo Calabresa.php

---
+++ Calabresa.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class Calabresa extends RecheioDecorator
+{
+    private $pao;
+
+    public function __construct(Pao $pao)
+    {
+        $this->pao = $pao;
+    }
+
+    public function getNome()
+    {
+        return $this->pao->getNome(). ", Calabresa";
+    }
+
+    public function valor() {
+        return 2 + $this->pao->valor();
+    }
+}
